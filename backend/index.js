b520ffa075080f446dbfd072becab11b50543f04
RubenU2002 <rsantiagourrego@gmail.com>
2023-01-21 17:06:49 -0500

    configuracion del sv con conex a mongodb

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app  = express();
+
+const stringConnection = 'mongodb://127.0.0.1:27017/prueba';
+
+mongoose.set("strictQuery", false);
+mongoose.connect(stringConnection)
+    .then(() => {
+      console.log("Conectado con exito a base de datos");
+    })
+    .catch((error) => {
+      console.log("Error al conectar a la base de datos ", error)
+    })
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(cors())
+
+const port = 3000;
+
+app.get("/",(req,res) => {
+    res.send("Servidor Backend");   
+})
+
+app.listen(port,()=>console.log(`Servidor backend escuchando en el puerto: ${port}`))
