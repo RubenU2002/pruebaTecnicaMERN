b81fe973d121b7db0d27e252ae2310e633d0c147
RubenU2002 <rsantiagourrego@gmail.com>
2023-01-21 18:00:42 -0500

    Configuración frontend y homepage

---
+++ frontend/src/screens/HomeSc.jsx
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom"
+const HomeSc = ()=>{
+    return(
+        <div>
+            <h1>Inicio</h1>
+            <div>
+                <Link to = "agregarPersona">Agregar Persona</Link>
+                <Link to= "/verPersonas">Ver Totalidad Personas</Link>
+                <Link to="/eliminarPersona">Eliminar Persona</Link>
+                <Link to="/actualizarPersona">Actualizar Persona</Link>
+            </div>
+        </div>
+    );
+}
+export default HomeSc;
